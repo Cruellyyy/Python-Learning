8118dcde47ae257c404bc9b442eb2deb0a37ee37
Cruellyyy <arshthe100@gmail.com>
2025-08-05 12:45:51 +0530

    variables and data types

---
+++ variables/main.py
@@ -0,0 +1,14 @@
+numberVariable = 42 # This is a positive integer variable
+negativeNumberVariable = -5 # This is a negative integer variable
+floatVariable = 3.14 # This is a floating-point variable
+stringVariable = "Hello, World!" # This is a string variable
+booleanVariable = True # This is a boolean variable
+booleanVariable2 = False # This is another boolean variable
+
+print("The number is:", numberVariable) 
+print("The string is:", stringVariable)
+
+trueVariable = True
+trueVariable = False # This will change the value of trueVariable to False
+print("The boolean variable is now:", trueVariable)
+print(type(trueVariable)) # This will print the type of trueVariable
