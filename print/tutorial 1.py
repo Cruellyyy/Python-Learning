03644f6ae95119d6755e854f1aca950f8a2e52f3
Cruellyyy <arshthe100@gmail.com>
2025-08-05 12:08:45 +0530

    print

---
+++ print/tutorial 1.py
@@ -0,0 +1,13 @@
+a = 10
+b= 20
+c = "This is a string"
+d = "and I am combining 2 strings"
+
+print("Hello world!")
+print ("This is a simple Python script.")
+print(9 + 10)
+print(9999)
+print(a + b)
+print (c)
+print(c + " " + d)
+
